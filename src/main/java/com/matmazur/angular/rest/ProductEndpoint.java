f496261bbb1d93c13176f113558926e4bcec8bc7
Bingo Bobo <matmazur90@gmail.com>
2018-10-22 13:54:56 +0200

    implemented rest endpoint for Products

--- src/main/java/com/matmazur/angular/rest/ProductEndpoint.java
+++ src/main/java/com/matmazur/angular/rest/ProductEndpoint.java
@@ -1,4 +1,58 @@
 package com.matmazur.angular.rest;
 
+
+import com.matmazur.angular.data.ProductRepository;
+import com.matmazur.angular.model.Product;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
+
+@RequestScoped
+@Path("/products")
+@Produces(MediaType.APPLICATION_JSON)
 public class ProductEndpoint {
+
+
+    @Inject
+    ProductRepository repository;
+
+    @GET
+    public Response getAllProducts() {
+
+
+        List<Product> list = repository.findAll();
+        if (list != null) {
+            return Response.ok(list).build();
+        } else {
+            return Response.noContent().build();
+        }
+    }
+
+
+    @GET
+    @Path("/{id}")
+    public Response getSpecificProduct(@PathParam("id") Long id) {
+
+        Product product = repository.findByID(id);
+        if (product != null) {
+            return Response.ok(product).build();
+        } else {
+            return Response.noContent().build();
+        }
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response saveProduct(Product product, @Context UriInfo uriInfo){
+
+        repository.save(product);
+        return Response.accepted(product).build();
+    }
+
 }
