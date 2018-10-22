aded4ddf0074ae56a3827fccade73ae9bf46c1b4
Bingo Bobo <matmazur90@gmail.com>
2018-10-22 14:00:24 +0200

    changed packaging to war, added additional property in persistence xml that will consume import.sql file and will create 3 records in DB

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO `test`.`product` (`kcal`, `name`, `producer`) VALUES ('666', 'Nutts', 'Felix');
+INSERT INTO `test`.`product` (`kcal`, `name`, `producer`) VALUES ('222', 'Pepsi', 'Pepsi Co');
+INSERT INTO `test`.`product` (`kcal`, `name`, `producer`) VALUES ('555', 'Cocoa powder', 'Coconuts');
